8e1aa7e5874fdee45573f9bbf1a4593161ce7b97
Rushikesh Jadhav <jadhavrushikesh283@gmail.com>
2025-05-19 12:56:41 +0530

    manifests added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(() => console.log("✅ Service Worker registered"))
+    .catch((err) => console.error("❌ SW registration failed:", err));
+}
+
 
 createRoot(document.getElementById("root")!).render(<App />);
